f782c6ce37356f99e337e75589fba73697305736
Camila Nepomuceno <camila.cno@gmail.com>
2022-11-29 17:28:29 -0300

    Radix for modal (accessible)

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import * as Dialog from '@radix-ui/react-dialog'
 import {
   HeaderContainer,
   HeaderContent,
@@ -12,8 +12,24 @@ export default function Header() {
     <HeaderContainer>
       <HeaderContent>
         <img src={logo} alt='' />
-        <NewTransactionButton>Nova transação</NewTransactionButton>
+
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>Nova transação</NewTransactionButton>
+          </Dialog.Trigger>
+
+          <Dialog.Portal>
+            <Dialog.Overlay />
+            <Dialog.Content>
+              <Dialog.Title>Nova transação</Dialog.Title>
+              <Dialog.Close />
+            </Dialog.Content>
+          </Dialog.Portal>
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   )
 }
+
+// asChild -> uses the application button instead of creating a new button as a trigger
+// portal -> render a child element elsewhere in the DOM
